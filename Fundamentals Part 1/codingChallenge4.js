b93677d34dadcfb234f39a2e2617c2e50223fa9c
Emma McCann <emccann1702@gmail.com>
2021-09-19 14:44:16 +0100

    Coding Challenge 4 complete

---
+++ Fundamentals Part 1/codingChallenge4.js
@@ -0,0 +1,6 @@
+// Coding Challenge 4
+const bill = 275;
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2; // ternary operator 
+
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value ${bill + tip}`);
